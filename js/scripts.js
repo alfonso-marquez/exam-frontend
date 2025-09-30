1448f447c8e1f2b2573e674f0ee39ba8dc777b83
alfonso.marquez <marquez.josealfonso@gmail.com>
2025-10-01 05:03:59 +0800

    Initial commit. Base proj and skeletal frontend.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 600,
+    fade: true,
+    cssEase: "linear",
+  });
+});
