60ae10c4a90586f4d4c40e85198ff25bffe57a83
alfonso.marquez <marquez.josealfonso@gmail.com>
2025-10-01 23:41:11 +0800

    Add dropdown. Add control on slick slider (WIP).

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
 $(document).ready(function () {
-  $(".slider").slick({
+  var $slider = $(".slider");
+
+  $slider.slick({
     dots: true,
     arrows: true,
     infinite: true,
     autoplay: true,
     autoplaySpeed: 3000,
     speed: 600,
-    fade: true,
-    cssEase: "linear",
+    appendDots: $(".custom-dots-div"), // Appends dots to this element
+  });
+
+  var isPlaying = true;
+
+  $(".play-pause-btn").on("click", function () {
+    if (isPlaying) {
+      $slider.slick("slickPause");
+      $(this).text("▶"); // play icon
+    } else {
+      $slider.slick("slickPlay");
+      $(this).text("❚❚"); // pause icon
+    }
+    isPlaying = !isPlaying;
   });
 });
